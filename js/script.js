38cc9642d224fcce60a0efb0fcc93d2cbece3cb9
Racheal Gathagu <rachealkari@gmail.com>
2022-01-03 20:07:13 +0300

    adding toggle effect on icons

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ $(document).ready(function() {
       $(".icons-hidden").toggle();
     });
   });
+
+$(document).ready(function () {
+    $("p#design").click(function () {
+        $("p#design").toggle();
+    });
+    $("p#development").click(function () {
+        $("p#development").toggle();
+    });
+    $("p#product").click(function () {
+        $("p#product").toggle();
+    });
+
+    $("ul#user").prepend("<li>Hello!</li>");
+        $("ul#webpage").prepend("<li>Why hello there!</li>");
+});
+
+  
