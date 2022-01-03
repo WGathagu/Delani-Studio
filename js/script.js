61861c70689db6d83873f82981347fb7ad02ffeb
Racheal Gathagu <rachealkari@gmail.com>
2022-01-03 22:18:54 +0300

    modifying icon toggle logic

--- js/script.js
+++ js/script.js
@@ -3,49 +3,78 @@
 $(document).ready(function () {
     $("p#design").click(function () {
         $("p#design").toggle();
-        $("p#des").show();
+        $("p#des").toggle();
+        $("p#des1").show();
         $("p#des_description").show();
         $("p#des_description").text("Design is the process designers use to build interfaces in software or computerized devices, focusing on looks or style. Designers aim to create interfaces which users find easy to use and pleasurable.");
-        $("p#des").css("font-weight", "bold");
+        $("p#des1").text("DESIGN");
+        $("p#des1").css("font-weight", "bold");
     });
-    /* $("p#des").click(function () {
-        $("p#des").toggle();
-        $("p#design").show();
-    }); */
-    $("p#des_description").click(function () {
+
+    $("p#des1").click(function () {
+        $("p#des1").toggle();
         $("p#des_description").toggle();
         $("p#design").show();
-        $("p#des").css("font-weight", "normal");
+        $("p#des").show();
+    });
+
+    $("p#des_description").click(function () {
+        $("p#des_description").toggle();
+        $("p#des1").toggle();
+        $("p#design").show();
+        $("p#des").show();
     });
 
     //Development
     $("p#development").click(function () {
         $("p#development").toggle();
-        $("p#dev").show();
+        $("p#dev").toggle();
+        $("p#dev1").show();
         $("p#dev_description").show();
         $("p#dev_description").text("development refers to a set of computer science activities dedicated to the process of creating, designing, deploying and supporting software.");
-        $("p#dev").css("font-weight", "bold");
+        $("p#dev1").text("DEVELOPMENT");
+        $("p#dev1").css("font-weight", "bold");
+    });
+    
+    $("p#dev1").click(function () {
+        $("p#dev1").toggle();
+        $("p#dev_description").toggle();
+        $("p#development").show();
+        $("p#dev").show();
     });
 
     $("p#dev_description").click(function () {
         $("p#dev_description").toggle();
+        $("p#dev1").toggle();
         $("p#development").show();
-        $("p#dev").css("font-weight", "normal");
+        $("p#dev").show();
+        
     });
 
     //Product
     $("p#product").click(function () {
         $("p#product").toggle();
-        $("p#prod").show();
+        $("p#prod").toggle();
+        $("p#prod1").show();
         $("p#prod_description").show();
         $("p#prod_description").text("Product management is an organisational function within a company dealing with new product development, business justification, planning, verification, forecasting, pricing, product launch, and marketing of a product or products at all stages of the product lifecycle.");
-        $("p#prod").css("font-weight", "bold");
+        $("p#prod1").text("PRODUCT MANAGEMENT");
+        $("p#prod1").css("font-weight", "bold");
+    });
+
+    $("p#prod1").click(function () {
+        $("p#prod1").toggle();
+        $("p#prod_description").toggle();
+        $("p#product").show();
+        $("p#prod").show();
     });
 
     $("p#prod_description").click(function () {
         $("p#prod_description").toggle();
+        $("p#prod1").toggle();
         $("p#product").show();
-        $("p#prod").css("font-weight", "normal");
+        $("p#prod").show();
+        
     });
 
     //Hover
